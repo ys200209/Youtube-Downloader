66ce703972375cab91055d78c698201d5c96aa74
ys200209 <lys200209@naver.com>
2022-04-06 00:25:32 +0900

    Android APP : Youtube Downloader

--- app/src/main/java/com/myDownload/example/MainActivity.java
+++ app/src/main/java/com/myDownload/example/MainActivity.java
@@ -43,52 +43,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initViews();
         initListeners();
-/*
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-        mAdView.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-                //Log.d("태그", "onAdLoaded");
-            }
-
-            @Override
-            public void onAdFailedToLoad(LoadAdError adError) {
-                // Code to be executed when an ad request fails.
-                //Log.d("태그", "onAdFailedToLoad " + adError);
-            }
-
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-            }
-
-            @Override
-            public void onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when the user is about to return
-                // to the app after tapping on an ad.
-            }
-        });*/
     }
 
     private void initListeners() {
